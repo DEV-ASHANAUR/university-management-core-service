d93c2e49ec0af4dc8aabe824121267ee356e5ef5
dev-ashanaur <devashanaur009@gmail.com>
2023-10-29 19:19:18 +0600

    [DNOE] student enrolled course mark

--- src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.controller.ts
+++ src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.controller.ts
@@ -1,19 +1,53 @@
-import { Request, Response } from "express";
-import catchAsync from "../../../shared/catchAsync";
-import { StudentEnrolledCourseMarkService } from "./studentEnrolledCourseMark.service";
-import sendResponse from "../../../shared/sendResponse";
-import httpStatus from "http-status";
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
+import sendResponse from '../../../shared/sendResponse';
+import { studentEnrolledCourseMarkFilterableFields } from './studentEnrolledCourseMark.constants';
+import { StudentEnrolledCourseMarkService } from './studentEnrolledCourseMark.service';
 
-const updateStudentMarks = catchAsync(async(req:Request,res:Response)=>{
-  const result = await StudentEnrolledCourseMarkService.updateStudentMarks(req.body);
-  sendResponse(res,{
-    statusCode:httpStatus.OK,
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, studentEnrolledCourseMarkFilterableFields);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+  const result = await StudentEnrolledCourseMarkService.getAllFromDB(
+    filters,
+    options
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
     success: true,
-    message: "marks updated!",
-    data: result
-  })
-})
+    message: 'Student course marks fetched successfully!',
+    data: result.data,
+    meta: result.meta,
+  });
+});
+
+const updateStudentMarks = catchAsync(async (req: Request, res: Response) => {
+  const result = await StudentEnrolledCourseMarkService.updateStudentMarks(
+    req.body
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'marks updated!',
+    data: result,
+  });
+});
+
+const updateFinalMarks = catchAsync(async (req: Request, res: Response) => {
+  const result = await StudentEnrolledCourseMarkService.updateFinalMarks(
+    req.body
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Final marks updated!',
+    data: result,
+  });
+});
 
 export const StudentEnrolledCourseMarkController = {
-  updateStudentMarks
-}
+  getAllFromDB,
+  updateStudentMarks,
+  updateFinalMarks,
+};
--- src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.route.ts
+++ src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.route.ts
@@ -1,8 +1,22 @@
-import express from 'express'
+import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import { StudentEnrolledCourseMarkController } from './studentEnrolledCourseMark.controller';
 
-const router = express.Router()
+const router = express.Router();
 
-router.patch("/update-marks",StudentEnrolledCourseMarkController.updateStudentMarks)
+router.patch(
+  '/',
+  auth(ENUM_USER_ROLE.FACULTY, ENUM_USER_ROLE.ADMIN),
+  StudentEnrolledCourseMarkController.getAllFromDB
+);
+router.patch(
+  '/update-marks',auth(ENUM_USER_ROLE.ADMIN,ENUM_USER_ROLE.FACULTY),
+  StudentEnrolledCourseMarkController.updateStudentMarks
+);
+router.patch(
+  '/update-final-marks',auth(ENUM_USER_ROLE.ADMIN,ENUM_USER_ROLE.FACULTY),
+  StudentEnrolledCourseMarkController.updateFinalMarks
+);
 
 export const studentEnrolledCourseMarkRoutes = router;
